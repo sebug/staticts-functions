227452d3f3f6c2a83bcf86061a63ac4f99f5d1e9
sebug <sebastian.gfeller@gmail.com>
2018-11-10 19:49:36 +0100

    Will it go through fast enough?

--- UploadLinesTrigger/index.js
+++ UploadLinesTrigger/index.js
@@ -1,36 +1,49 @@
 const azure = require('azure-storage');
 
-function storeLines(lines, context) {
+function storeEntity(tableService, entity) {
     return new Promise((resolve, reject) => {
-	const tableService = azure.createTableService();
-	const entGen = azure.TableUtilities.entityGenerator;
-
-	const partition = 'prod';
-
-	const entities = lines.map(line => {
-	    return {
-		PartitionKey: entGen.String(partition),
-		RowKey: entGen.String(line.lineNumber),
-		StartDate: entGen.String(line.startDate),
-		EndDate: entGen.String(line.endDate),
-		JobNumber: entGen.String(line.jobNumber),
-		JobName: entGen.String(line.jobName),
-		ClientNumber: entGen.String(line.clientNumber),
-		ClientName: entGen.String(line.clientName),
-		TaskNumber: entGen.String(line.taskNumber),
-		TaskDescription: entGen.String(line.taskDescription),
-		SiteCode: entGen.String(line.siteCode),
-		WorkTypeCode: entGen.String(line.workTypeCode),
-		WorkTypeDescription: entGen.String(line.workTypeDescription),
-		QuantityWorked: entGen.Double(line.quantityWorked),
-		InvoiceQuantity: entGen.Double(line.invoiceQuantity)
-	    };
+	tableService.insertOrReplaceEntity('timesheetLines', entity, (error, result, response) => {
+	    if (!error) {
+		resolve(result);
+	    } else {
+		reject(error);
+	    }
 	});
-	
-	context.log('Got ' + lines.length + ' lines!');
-	context.log(JSON.stringify(entities[0]));
-	resolve(true);
     });
+}
+
+async function storeLines(lines, context) {
+    const tableService = azure.createTableService();
+    const entGen = azure.TableUtilities.entityGenerator;
+
+    const partition = 'prod';
+
+    const entities = lines.map(line => {
+	return {
+	    PartitionKey: entGen.String(partition),
+	    RowKey: entGen.String(line.lineNumber),
+	    StartDate: entGen.String(line.startDate),
+	    EndDate: entGen.String(line.endDate),
+	    JobNumber: entGen.String(line.jobNumber),
+	    JobName: entGen.String(line.jobName),
+	    ClientNumber: entGen.String(line.clientNumber),
+	    ClientName: entGen.String(line.clientName),
+	    TaskNumber: entGen.String(line.taskNumber),
+	    TaskDescription: entGen.String(line.taskDescription),
+	    SiteCode: entGen.String(line.siteCode),
+	    WorkTypeCode: entGen.String(line.workTypeCode),
+	    WorkTypeDescription: entGen.String(line.workTypeDescription),
+	    QuantityWorked: entGen.Double(line.quantityWorked),
+	    InvoiceQuantity: entGen.Double(line.invoiceQuantity)
+	};
+    });
+
+    for (let entity of entities) {
+	const importResult = await storeEntity(tableService, entity);
+	context.log('Saved ' + importResult.RowKey._);
+    }
+    
+    return true;
 }
 
 module.exports = async function (context, req) {
