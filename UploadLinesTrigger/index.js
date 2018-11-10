8d8b04129d26b84ca5a61646bdb4dfaeaf6995d4
sebug <sebastian.gfeller@gmail.com>
2018-11-10 18:27:56 +0100

    Logging first line

--- UploadLinesTrigger/index.js
+++ UploadLinesTrigger/index.js
@@ -5,6 +5,7 @@ module.exports = function (context, req) {
 	context.log('Did not receive a body');
     } else {
 	context.log('Got ' + req.body.length + ' lines');
+	context.log(JSON.stringify(req.body[0]));
     }
     context.res = {
 	body: 'Uploaded',
