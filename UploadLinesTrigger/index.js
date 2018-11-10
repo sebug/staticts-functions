82bf482a24434326b51acb600a899cb94df0dc0f
sebug <sebastian.gfeller@gmail.com>
2018-11-10 18:24:50 +0100

    Logging how many lines we get

--- UploadLinesTrigger/index.js
+++ UploadLinesTrigger/index.js
@@ -1,6 +1,11 @@
 
 module.exports = function (context, req) {
     context.log('Uploading file');
+    if (!req.body || !req.body.length) {
+	context.log('Did not receive a body');
+    } else {
+	context.log('Got ' + req.body.length + ' lines');
+    }
     context.res = {
 	body: 'Uploaded',
 	status: 200
