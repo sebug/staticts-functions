f45728894014829022e5db515b97e9eb2e64f9c4
sebug <sebastian.gfeller@gmail.com>
2018-11-10 18:38:33 +0100

    Getting ready for azure storage

--- UploadLinesTrigger/index.js
+++ UploadLinesTrigger/index.js
@@ -1,14 +1,16 @@
+const azure = require('azure-storage');
+
+function storeLines(lines, context) {
+    context.log('Got ' + lines.length + ' lines!');
+    context.log(JSON.stringify(lines[0]));
+}
 
 module.exports = function (context, req) {
     context.log('Uploading file');
     if (!req.body || !req.body.length) {
 	context.log('Did not receive a body');
     } else {
-	context.log('Got ' + req.body.length + ' lines');
-	context.log(JSON.stringify(req.body[0]));
-	context.log('trying to get a date');
-	let d = new Date(req.body[0].startDate);
-	context.log(d);
+	storeLines(req.body, context);
     }
     context.res = {
 	body: 'Uploaded',
