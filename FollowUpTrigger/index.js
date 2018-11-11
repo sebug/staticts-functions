3c62472b5b9b5c02c074e42ddac907ef17dfc619
sebug <sebastian.gfeller@gmail.com>
2018-11-11 13:50:25 +0100

    Logging gist URL

--- FollowUpTrigger/index.js
+++ FollowUpTrigger/index.js
@@ -12,6 +12,7 @@ module.exports = async function (context, req) {
     context.log('Calculating summary');
     const summary = await calculateSummary();
     context.log('Summary result is ' + JSON.stringify(summary));
+    context.log('Gist url is ' + process.env.GIST_URL);
     return {
         body: summary,
         status: 200
