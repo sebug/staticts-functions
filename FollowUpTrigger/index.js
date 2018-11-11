cf960c3fcadd5de7a85be11590b84112a16e2318
sebug <sebastian.gfeller@gmail.com>
2018-11-11 14:00:55 +0100

    Fetching estimation from gist

--- FollowUpTrigger/index.js
+++ FollowUpTrigger/index.js
@@ -1,16 +1,34 @@
 const azure = require('azure-storage');
+const https = require('https');
 
-function calculateSummary() {
+function getEstimation(context) {
     return new Promise((resolve, reject) => {
-	resolve({
-	    name: 'Sebastian Gfeller'
+	https.get(process.env.GIST_URL, (resp) => {
+	    let data = '';
+
+	    resp.on('data', (chunk) => {
+		data += chunk;
+	    });
+	    
+	    resp.on('end', () => {
+		const estimationResult = JSON.parse(data);
+
+		context.log('got ' + estimationResult.EstimationLines.length + ' estimation lines');
+
+		resolve(estimationResult);
+	    });
 	});
     });
 }
 
+async function calculateSummary(context) {
+    const estimation = await getEstimation(context);
+    return estimation;
+}
+
 module.exports = async function (context, req) {
     context.log('Calculating summary');
-    const summary = await calculateSummary();
+    const summary = await calculateSummary(context);
     context.log('Summary result is ' + JSON.stringify(summary));
     context.log('Gist url is ' + process.env.GIST_URL);
     return {
