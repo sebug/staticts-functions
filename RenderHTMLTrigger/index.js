4089462a99653bb3be944cc56bca7d791b4f7a19
sebug <sebastian.gfeller@gmail.com>
2018-11-18 12:57:11 +0100

    Adding initial trigger for HTML rendering

---
+++ RenderHTMLTrigger/index.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+module.exports = async function (context, req) {
+    context.log('rendering follow-up page');
+    return {
+	body: '<!DOCTYPE html>' +
+	    '<html>' +
+	    '<head>' +
+	    '<title>Pre-rendered follow-up</title>' +
+	    '<meta charset="utf-8">' +
+	    '</head>' +
+	    '<body>' +
+	    '<h1>Pre-rendered follow-up</h1>' +
+	    '</body>' +
+	    '</html>',
+	status: 200,
+	headers: {
+	    'Content-Type': 'text/html'
+	}
+    };
+};
